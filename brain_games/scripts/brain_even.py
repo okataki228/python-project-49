619d179dcbb83c2f76ff6cbf694c24abb422a95f
Leon Podlesnij <leonpodlesny@gmail.com>
2025-02-28 16:16:47 +0200

    lier game add

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,28 @@
+import random
+
+def is_even(number):
+    return number % 2 == 0
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    rounds_to_win = 3
+    for _ in range(rounds_to_win):
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        answer = input("Your answer: ").strip().lower()
+
+        correct_answer = "yes" if is_even(number) else "no"
+
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
