93cb62f4905476411bd6634a0034fb12c59326e3
Leon Podlesnij <leonpodlesny@gmail.com>
2025-03-01 13:22:20 +0200

    Добавлена игра 'НОД', Удалены лишние файлы

---
+++ brain_games/engine.py
@@ -0,0 +1,23 @@
+import prompt
+
+ROUNDS_COUNT = 3
+
+def run_game(rules, generate_question_and_answer):
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print(rules)
+
+    for _ in range(ROUNDS_COUNT):
+        question, correct_answer = generate_question_and_answer()
+        print(f"Question: {question}")
+        user_answer = prompt.string("Your answer: ")
+
+        if user_answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,18 @@
+import random
+import math
+from brain_games.engine import run_game
+
+RULES = "Find the greatest common divisor of given numbers."
+
+def generate_question_and_answer():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    question = f"{num1} {num2}"
+    correct_answer = str(math.gcd(num1, num2))
+    return question, correct_answer
+
+def main():
+    run_game(RULES, generate_question_and_answer)
+
+if __name__ == "__main__":
+    main()
